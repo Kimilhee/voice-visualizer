a91bfa62a4ed74c7b372df6d8b7bedac36e25ab5
Kimilhee <ilhee.kim@knowre.com>
2025-05-09 11:43:02 +0900

    포트 8001로 지정.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8001, // ← 이 값이 명령줄보다 우선 적용됨
+    strictPort: true,
+  },
 })
