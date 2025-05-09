add62c49a06b2b802b31ef5eacc9570fab65ee5a
ilhee <iamarej@gmail.com>
2025-05-09 05:42:17 +0000

    host 0.0.0.0 으로 뜨게 수정.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   preview: {
+    host: "0.0.0.0",
     port: 8001, // ← 이 값이 명령줄보다 우선 적용됨
     strictPort: true,
   },
